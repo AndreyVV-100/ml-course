dd81f74a27ffd29135105c4abfdcf5abdce589b0
Andrey Vyazovtsev <viazovtsev.av@phystech.edu>
2023-10-16 02:33:59 +0300

    Add HW4.

--- homeworks/hw04_power_iteration/power_iteration.py
+++ homeworks/hw04_power_iteration/power_iteration.py
@@ -10,5 +10,9 @@ def get_dominant_eigenvalue_and_eigenvector(data, num_steps):
     eigenvector: np.ndarray – corresponding eigenvector estimation
     """
     ### YOUR CODE HERE
+    x_n = np.random.rand(data.shape[0])
+    for i in range(num_steps):
+        A_x_n = data.dot(x_n)
+        x_n = A_x_n / np.sqrt(np.sum(A_x_n * A_x_n))
 
-    return 
+    return float(np.dot(x_n, data.dot(x_n)) / np.dot(x_n, x_n)), x_n
